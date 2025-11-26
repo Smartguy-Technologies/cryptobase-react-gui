a37bf5785333646239043e2a4b0fde862864c421
iAmJamesGardner <36855868+iAmJamesGardner@users.noreply.github.com>
2025-11-26 14:05:30 -0500

    Refactor BuyScreen to include a View wrapper
    Signed-off-by: iAmJamesGardner <36855868+iAmJamesGardner@users.noreply.github.com>

--- src/components/scenes/MoonPayBuyScene.tsx
+++ src/components/scenes/MoonPayBuyScene.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import { WebView } from 'react-native-webview';
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 export default function BuyScreen() {
   return (
-    <WebView
-      source={{ uri: 'https://buy.moonpay.com?apiKey=pk_live_NWOV9M89TmiXNafI1qWZfhQG4eOk3Q2&theme=dark' }}
-
-      style={{ flex: 1, marginTop: 55, marginBottom: 55 }}
-    />
-  );
-
-};
+    <View style={{ flex: 1 }}>
+      <WebView
+        source={{
+          uri: 'https://buy.moonpay.com?apiKey=pk_live_NWOV9M89TmiXNafI1qWZfhQG4eOk3Q2&theme=dark'
+          //uri: 'https://sell.moonpay.com?apiKey=pk_live_NWOV9M89TmiXNafI1qWZfhQG4eOk3Q2&theme=dark'
+        }}
+        style={{ flex: 1, marginTop: 120, marginBottom: 90 }}
+      />
+    </View>
+  )
+}
