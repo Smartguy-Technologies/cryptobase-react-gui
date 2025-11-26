7aa9228ba69ea806a6db0d3e12f5ab4f58be8688
iAmJamesGardner <36855868+iAmJamesGardner@users.noreply.github.com>
2025-11-26 14:05:48 -0500

    Refactor SellScreen to BuyScreen with updated WebView
    Signed-off-by: iAmJamesGardner <36855868+iAmJamesGardner@users.noreply.github.com>

--- src/components/scenes/MoonPaySellScene.tsx
+++ src/components/scenes/MoonPaySellScene.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { WebView } from 'react-native-webview';
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { WebView } from 'react-native-webview'
 
-export default function SellScreen() {
-    return (
-        <WebView
-            //   source={{ uri: 'https://sell-sandbox.moonpay.com?apiKey=pk_test_TE5hdy2bEL4mvbWtZXsNV16iEhDB0xq&baseCurrencyCode=eth&refundWalletAddress=0xb34175CbA06b810366431eBf7cf74C38de4249E7&theme=dark&baseCurrencyAmount=0.05&email=developers@cryptobaseatm.com' }}
-            source={{ uri: 'https://sell.moonpay.com?apiKey=pk_live_NWOV9M89TmiXNafI1qWZfhQG4eOk3Q2&theme=dark' }}
-
-            style={{ flex: 1, marginTop: 55, marginBottom: 55 }}
-        />
-    );
-
-};
+export default function BuyScreen() {
+  return (
+    <View style={{ flex: 1 }}>
+      <WebView
+        source={{
+          //uri: 'https://buy.moonpay.com?apiKey=pk_live_NWOV9M89TmiXNafI1qWZfhQG4eOk3Q2&theme=dark'
+          uri: 'https://sell.moonpay.com?apiKey=pk_live_NWOV9M89TmiXNafI1qWZfhQG4eOk3Q2&theme=dark'
+        }}
+        style={{ flex: 1, marginTop: 120, marginBottom: 90 }}
+      />
+    </View>
+  )
+}
